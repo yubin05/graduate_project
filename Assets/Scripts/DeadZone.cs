73a89b0b658b824bb622528a2a62368ddaaa6f65
yubin <vundle34@naver.com>
2023-05-25 14:06:48 +0900

    dead zone and camera destroy edit

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerController>().Dead();
+        }
+    }
+}
