e91ce7175768786754175a9e56cb7873b5a0f81a
_ Guest_ <vundle34@naver.com>
2023-10-31 01:28:35 +0900

    슬라임 벽 버그 수정

--- Assets/Scripts/Stage/Platform.cs
+++ Assets/Scripts/Stage/Platform.cs
@@ -7,17 +7,10 @@ public class Platform : MonoBehaviour
     protected GameObject player;
     protected PlayerController player_script;
 
-    /*protected readonly string parentClassName = "Platform";
-    protected IEnumerator IEAfterLanding;
-    protected WaitForSeconds waitForSeconds;*/
-
     protected virtual void Start()
     {
         player = GameObject.FindWithTag("Player");
         player_script = player.GetComponent<PlayerController>();
-
-        /*IEAfterLanding = AfterLanding();
-        waitForSeconds = new WaitForSeconds(1f);*/
     }
 
     protected virtual void OnCollisionEnter2D(Collision2D collision)
@@ -28,11 +21,6 @@ public class Platform : MonoBehaviour
             //Debug.Log("Landing");
             player_script.Landing();
         }
-
-        /*if (GetType().Name == parentClassName)
-        {
-            StartCoroutine(IEAfterLanding);
-        }*/
             
     }
     protected virtual void OnCollisionExit2D(Collision2D collision)
@@ -44,12 +32,4 @@ public class Platform : MonoBehaviour
             player_script.isGrounded = false;
         }
     }
-
-    /*// platform, floor 겹침으로 일어나는 버그 방지 위한 코루틴
-    protected IEnumerator AfterLanding()
-    {
-        yield return waitForSeconds;
-        Debug.Log("AfterLanding");
-        player_script.isGrounded = true;
-    }*/
 }
